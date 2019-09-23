e30415c9ce4bedb95732a534e94e19635ec4ddb0
Rama Al Halik <55701022+RamaA-H@users.noreply.github.com>
2019-09-23 21:46:55 +0700

    Add files via upload

---
+++ PBO1-10118007-Latihan9-Teks/src/pbo1/pkg10118007/latihan9/teks/PBO110118007Latihan9Teks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118007.latihan9.teks;
+
+/**
+ *
+ * @author Rama
+ * Nama   : Rama Al Halik
+ * Kelas  : PBO1
+ * NIM    : 10118007
+ * Deskripsi Program : Menampilkan penggunaan tipe data char
+ */
+public class PBO110118007Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "char : " + tab + "berisi " + c);
+    }
+    
+}
