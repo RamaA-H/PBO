efb23743a8bc821e091328e31fcc17d32f256df7
Rama Al Halik <55701022+RamaA-H@users.noreply.github.com>
2019-09-23 21:44:49 +0700

    Add files via upload

---
+++ PBO1-10118007-Latihan7-Integer/src/pbo1/pkg10118007/latihan7/integer/PBO110118007Latihan7Integer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118007.latihan7.integer;
+
+/**
+ *
+ * @author Rama
+ * Nama   : Rama Al Halik
+ * Kelas  : PBO1
+ * NIM    : 10118007
+ * Deskripsi Program : Menampilkan Output Integer
+ */
+public class PBO110118007Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
