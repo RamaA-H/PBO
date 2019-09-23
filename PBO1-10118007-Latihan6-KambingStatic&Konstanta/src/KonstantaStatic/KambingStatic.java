d4f5e7376893d89793da8391becdbd8d6c4c3baa
Rama Al Halik <55701022+RamaA-H@users.noreply.github.com>
2019-09-23 21:44:10 +0700

    Add files via upload

---
+++ PBO1-10118007-Latihan6-KambingStatic&Konstanta/src/KonstantaStatic/KambingStatic.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KonstantaStatic;
+
+/**
+ *
+ * @author Rama
+ * Nama   : Rama Al Halik
+ * Kelas  : PBO1
+ * NIM    : 10118007
+ * Deskripsi Program : Menampilkan  variabel statik 
+ */
+public class KambingStatic {
+    
+  //NAMA_KAMBING sebagai Konstanta
+    public static final String NAMA_KAMBING = "MIDUN ";
+    
+    public static void main (String[] args){
+          Mamalia.jumlahKambing = 485000;
+          System.out.println(NAMA_KAMBING + "memiliki kambing sebanyak " + Mamalia.jumlahKambing);
+    }
+}
+
---
+++ PBO1-10118007-Latihan6-KambingStatic&Konstanta/src/KonstantaStatic/Mamalia.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KonstantaStatic;
+
+/**
+ *
+ * @author Rama
+ * Nama   : Rama Al Halik
+ * Kelas  : PBO1
+ * NIM    : 10118007
+ * Deskripsi Program : Variabel yang dideklarasikan sebagai statik
+ */
+public class Mamalia {
+     // Variael jumlahKambing dideklarasikan sebagai statik
+    public static int jumlahKambing;
+}
+
