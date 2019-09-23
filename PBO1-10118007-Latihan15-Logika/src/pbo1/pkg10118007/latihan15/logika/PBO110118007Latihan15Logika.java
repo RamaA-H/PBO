37ba404c6771cec57ee6b9469b0db8317f167983
Rama Al Halik <55701022+RamaA-H@users.noreply.github.com>
2019-09-23 21:53:33 +0700

    Add files via upload

---
+++ PBO1-10118007-Latihan15-Logika/src/pbo1/pkg10118007/latihan15/logika/PBO110118007Latihan15Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118007.latihan15.logika;
+
+/**
+ *
+ * @author Rama
+ * Nama   : Rama Al Halik
+ * Kelas  : PBO1
+ * NIM    : 10118007
+ * Deskripsi Program : Menampilkan contoh Logika pada Java
+ */
+public class PBO110118007Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
