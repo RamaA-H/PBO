7f0dc362c5bdc48358d4c5484c6218484c1962f7
Rama Al Halik <55701022+RamaA-H@users.noreply.github.com>
2019-09-23 21:30:05 +0700

    Add files via upload

---
+++ PBO1-10118007-Latihan1-MenampilkanValue/src/pbo1/pkg10118007/latihan1/menampilkanvalue/PBO110118007Latihan1MenampilkanValue.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118007.latihan1.menampilkanvalue;
+
+/**
+ *
+ * @author Rama
+ * Nama   : Rama Al Halik
+ * Kelas  : PBO1
+ * NIM    : 10118007
+ * Deskripsi Program :Program ini berisi value yang ingin di tampilkan
+ */
+public class PBO110118007Latihan1MenampilkanValue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       System.out.println("BIODATA " + "\n" + "------------------");
+       System.out.println("NAMA\t\t         : Rama Al Halik");
+       System.out.println("Alamat\t\t         : Jalan Dago Barat");
+       System.out.println("Tempat Lahir\t\t : Tanjung Pandan");
+       System.out.println("Tanggal Lahir\t\t : 21 Desember 2000");
+       System.out.println("------------------");
+       System.out.println("Hobi\t\t : Main & Tidur");
+       System.out.println("Makes\t\t : Rendang");
+       System.out.println("Mikes\t\t : Minuman Dingin");
+    }
+    
+}
