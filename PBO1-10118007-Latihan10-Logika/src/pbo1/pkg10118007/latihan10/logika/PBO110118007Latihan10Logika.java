e561b5fba4c48eef1554891f0064c0d73b7915c9
Rama Al Halik <55701022+RamaA-H@users.noreply.github.com>
2019-09-23 21:47:53 +0700

    Add files via upload

---
+++ PBO1-10118007-Latihan10-Logika/src/pbo1/pkg10118007/latihan10/logika/PBO110118007Latihan10Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118007.latihan10.logika;
+
+/**
+ *
+ * @author Rama
+ * Nama   : Rama Al Halik
+ * Kelas  : PBO1
+ * NIM    : 10118007
+ * Deskripsi Program : Menampilkan penggunaan Tipe data Boolean
+ */
+public class PBO110118007Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int umur = 19;
+       boolean dewasa;
+       dewasa = (umur >= 17);
+       System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
