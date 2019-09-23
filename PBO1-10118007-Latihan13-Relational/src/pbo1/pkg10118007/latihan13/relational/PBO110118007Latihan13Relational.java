c641e6826bf278ea1cc8e02f49c9ff7a0cb4cd99
Rama Al Halik <55701022+RamaA-H@users.noreply.github.com>
2019-09-23 21:50:33 +0700

    Add files via upload

---
+++ PBO1-10118007-Latihan13-Relational/src/pbo1/pkg10118007/latihan13/relational/PBO110118007Latihan13Relational.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118007.latihan13.relational;
+
+/**
+ *
+ * @author Rama
+ * Nama   : Rama Al Halik
+ * Kelas  : PBO1
+ * NIM    : 10118007
+ * Deskripsi Program : Menampilkan contoh Relasi pada Java
+ */
+public class PBO110118007Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("a >= b = " + (bil1 >= bil2));
+        System.out.println("a <= b = " + (bil1 <= bil2));
+    }
+    
+}
